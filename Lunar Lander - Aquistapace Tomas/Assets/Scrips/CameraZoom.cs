2ede1fbe4881cae5f1fb902279c48d32762d8e9a
Tomas-Aquistapace <Aquista35@gmail.com>
2021-06-25 20:53:56 -0300

    Added Basic Movement

---
+++ Lunar Lander - Aquistapace Tomas/Assets/Scrips/CameraZoom.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    public Transform playerRef;
+    public Vector3 offset;
+    [SerializeField]
+    bool enableZoom = false;
+    bool followPlayer = false;
+
+
+    Camera cam;
+    float zoomLimit;
+
+    void Start()
+    {
+        cam = transform.GetComponent<Camera>();
+
+        zoomLimit = playerRef.GetComponent<Player_Ship>().limitAltitude;
+    }
+
+    void Update()
+    {
+        Zoom();
+        FollowPlayer();
+    }
+
+    void FollowPlayer()
+    {
+        if (followPlayer)
+        {
+            Vector3 desiredPos = playerRef.position + offset;
+            transform.position = Vector3.Lerp(transform.position, desiredPos, 0.125f);
+        }
+    }
+
+    void Zoom()
+    {
+        if(playerRef.transform.position.y <= 0 && enableZoom == false)
+        {
+            StartCoroutine(TranslateToPlayer());
+        }
+        else if(playerRef.transform.position.y > 0 && enableZoom == true)
+        {
+            enableZoom = false;
+            followPlayer = false;
+        }
+    }
+
+    IEnumerator TranslateToPlayer()
+    {
+        enableZoom = true;
+        float time = 0;
+
+        while (time <= 1)
+        {
+            Vector3 desiredPos = playerRef.position + offset;
+
+            transform.position = Vector3.Lerp(transform.position, desiredPos, time);
+
+            time += 0.2f * Time.deltaTime;
+            yield return null;
+        }
+
+        followPlayer = true;
+    }
+}
---
+++ Lunar Lander - Aquistapace Tomas/Assets/Scrips/Player_Ship.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Ship : MonoBehaviour
+{
+    [Header("Stats")]
+    public float speedRotation = 2f;
+    public float forceImpulse = 2f;
+    public float actualAltitude = 0f;
+
+    [Header("External References")]
+    public float limitAltitude = -35f;
+
+    Rigidbody2D rig;
+
+    private void Start()
+    {
+        actualAltitude = (int)transform.position.y;
+
+        rig = transform.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        InputRotation();
+        ForceImpulse();
+        UpdateAltitude();
+    }
+
+    void InputRotation()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        transform.Rotate(new Vector3(0, 0, -horizontal), speedRotation * Time.deltaTime);
+    }
+
+    void ForceImpulse()
+    {
+        if (Input.GetKey("space"))
+        {
+            rig.AddForce(transform.up * forceImpulse, ForceMode2D.Force);
+        }
+    }
+
+    void UpdateAltitude()
+    {
+        actualAltitude = transform.position.y;
+    }
+}
